5863490d6f5d8676067270e25adcddf44a06d76a
jan-grasewicz <45798702+jan-grasewicz@users.noreply.github.com>
2021-11-27 23:47:07 +0100

    envs and google apis setup

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const { google } = require('googleapis')
+
 const app = express()
-const PORT = 8080
 
 app.use(express.json())
 
-app.listen(PORT, () => console.log(`live on port: ${PORT}`))
+app.listen(process.env.PORT, () => console.log(`live on port: ${process.env.PORT}`))
 
-app.get('/test', (req, res) => {
-  res.status(200).send({ message: 'hello' })
+app.get('/test', async (req, res) => {
+  console.log('sheet', process.env.SHEET_ID)
+  const auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+  })
+
+  const sheets = google.sheets({ version: 'v4', auth })
+
+  const sheetsRes = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range: 'Sheet1!A2:A10',
+    majorDimension: 'COLUMNS',
+  })
+
+  res.status(200).send({ data: sheetsRes })
 })
