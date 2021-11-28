cdc0165026f51b055a926e3c1b63c0e0e2766e5b
jan-grasewicz <45798702+jan-grasewicz@users.noreply.github.com>
2021-11-28 17:00:25 +0100

    initial endpoints added

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const app = express()
 
 app.use(express.json())
 
-app.listen(process.env.PORT, () => console.log(`live on port: ${process.env.PORT}`))
-
 app.get('/test', async (req, res) => {
   console.log('sheet', process.env.SHEET_ID)
   const auth = await google.auth.getClient({
@@ -23,3 +21,31 @@ app.get('/test', async (req, res) => {
 
   res.status(200).send({ data: sheetsRes })
 })
+
+// fetch topics with their metadata
+app.get('/topics', (req, res) => {
+  res.status(200).send('WIP')
+})
+
+// fetch topic elements list
+app.get('/elements/:elements_list_id', (req, res) => {
+  res.status(200).send(`WIP, ${req.params.elements_list_id}`)
+})
+
+// save new element
+app.post('/elements/:elements_list_id', (req, res) => {
+  console.log('> ~ req', req)
+  res.status(200).send(`WIP, ${req.params.elements_list_id}, ${req.body.data}`)
+})
+
+// fetch topic entries list
+app.get('/entries/:entries_list_id', (req, res) => {
+  res.status(200).send(`WIP, ${req.params.entries_list_id}`)
+})
+
+// save new entry
+app.post('/entries/:entries_list_id', (req, res) => {
+  res.status(200).send(`WIP, ${req.params.entries_list_id}, ${req.body.data}`)
+})
+
+app.listen(process.env.PORT, () => console.log(`live on port: ${process.env.PORT}`))
