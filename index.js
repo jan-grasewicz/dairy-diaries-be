3a4800412ce74aa52062b3f80074734951b7bd8a
jan-grasewicz <45798702+jan-grasewicz@users.noreply.github.com>
2021-12-05 11:56:47 +0100

    get elements and entries endpoints added

--- index.js
+++ index.js
@@ -11,29 +11,53 @@ const getSheets = async () => {
   })
 
   const sheets = google.sheets({ version: 'v4', auth })
+
   return sheets
 }
 
-// fetch topics with their metadata
-app.get('/topics', async (req, res) => {
+const readSheet = async (range) => {
   const sheets = await getSheets()
   const sheetsRes = await sheets.spreadsheets.values.get({
     spreadsheetId: process.env.SHEET_ID,
-    range: 'topics!A1:E',
+    range,
   })
 
-  const data = sheetsRes.data.values?.reduce((acc, cur, i, arr) => {
+  return sheetsRes
+}
+
+const getRowData = (arr) =>
+  arr.reduce((acc, cur, i, arr) => {
     if (i === 0) return acc
     const rowData = cur.reduce((accu, curr, idx) => ({ ...accu, [arr[0][idx]]: curr }), {})
     return [...acc, rowData]
   }, [])
 
-  res.status(200).send({ data })
+// fetch topics with their metadata
+app.get('/topics', async (_req, res) => {
+  try {
+    const sheetsRes = await readSheet(`topics!A1:${process.env.TOPICS_LAST_COL}`)
+
+    const data = getRowData(sheetsRes.data.values)
+
+    res.status(200).send({ data })
+  } catch (e) {
+    res.status(e.code).send({ data: e.response.data.error })
+  }
 })
 
 // fetch topic elements list
 app.get('/elements/:elements_list_id', async (req, res) => {
-  res.status(200).send(`WIP, ${req.params.elements_list_id}`)
+  try {
+    const sheetsRes = await readSheet(
+      `${req.params.elements_list_id}!A1:${process.env.ELEMENTS_LAST_COL}`
+    )
+
+    const data = getRowData(sheetsRes.data.values)
+
+    res.status(200).send({ data })
+  } catch (e) {
+    res.status(e.code).send({ data: e.response.data.error })
+  }
 })
 
 // save new element
@@ -44,7 +68,17 @@ app.post('/elements/:elements_list_id', async (req, res) => {
 
 // fetch topic entries list
 app.get('/entries/:entries_list_id', async (req, res) => {
-  res.status(200).send(`WIP, ${req.params.entries_list_id}`)
+  try {
+    const sheetsRes = await readSheet(
+      `${req.params.entries_list_id}!A1:${process.env.ENTRIES_LAST_COL}`
+    )
+
+    const data = getRowData(sheetsRes.data.values)
+
+    res.status(200).send({ data })
+  } catch (e) {
+    res.status(e.code).send({ data: e.response.data.error })
+  }
 })
 
 // save new entry
