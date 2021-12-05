44f6ed2630c3ac91acba666ab721c1787fb5987f
jan-grasewicz <45798702+jan-grasewicz@users.noreply.github.com>
2021-12-05 11:02:03 +0100

    get topics endpoint added

--- index.js
+++ index.js
@@ -5,46 +5,50 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/test', async (req, res) => {
-  console.log('sheet', process.env.SHEET_ID)
+const getSheets = async () => {
   const auth = await google.auth.getClient({
     scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
   })
 
   const sheets = google.sheets({ version: 'v4', auth })
-
-  const sheetsRes = await sheets.spreadsheets.values.get({
-    spreadsheetId: process.env.SHEET_ID,
-    range: 'Sheet1!A2:A10',
-    majorDimension: 'COLUMNS',
-  })
-
-  res.status(200).send({ data: sheetsRes })
-})
+  return sheets
+}
 
 // fetch topics with their metadata
-app.get('/topics', (req, res) => {
-  res.status(200).send('WIP')
+app.get('/topics', async (req, res) => {
+  const sheets = await getSheets()
+  const sheetsRes = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range: 'topics!A1:E',
+  })
+
+  const data = sheetsRes.data.values?.reduce((acc, cur, i, arr) => {
+    if (i === 0) return acc
+    const rowData = cur.reduce((accu, curr, idx) => ({ ...accu, [arr[0][idx]]: curr }), {})
+    return [...acc, rowData]
+  }, [])
+
+  res.status(200).send({ data })
 })
 
 // fetch topic elements list
-app.get('/elements/:elements_list_id', (req, res) => {
+app.get('/elements/:elements_list_id', async (req, res) => {
   res.status(200).send(`WIP, ${req.params.elements_list_id}`)
 })
 
 // save new element
-app.post('/elements/:elements_list_id', (req, res) => {
+app.post('/elements/:elements_list_id', async (req, res) => {
   console.log('> ~ req', req)
   res.status(200).send(`WIP, ${req.params.elements_list_id}, ${req.body.data}`)
 })
 
 // fetch topic entries list
-app.get('/entries/:entries_list_id', (req, res) => {
+app.get('/entries/:entries_list_id', async (req, res) => {
   res.status(200).send(`WIP, ${req.params.entries_list_id}`)
 })
 
 // save new entry
-app.post('/entries/:entries_list_id', (req, res) => {
+app.post('/entries/:entries_list_id', async (req, res) => {
   res.status(200).send(`WIP, ${req.params.entries_list_id}, ${req.body.data}`)
 })
 
